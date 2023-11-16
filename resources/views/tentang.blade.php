665450811544ca53df7fad70d64af9ecb4c6ce9f
Kautzar Randra Noor Khasyyatullah <kautzarrandra@gmail.com>
2023-11-16 15:19:33 +0700

    week 12 (16/11) pwebc : laravel

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('judulhalaman', 'About')
+
+@section('judul')
+<div class="jumbotron">
+    About me!
+</div>
+@endsection
+
+@section('konten')
+<h3>Isi Halaman About</h3>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('halo2', function () {
     return "<h1>halo apa kabar</h1>";
 });
 
-Route::get('blog', function () {
+Route::get('blog1', function () {
     return view('blog');
 });
 
@@ -61,6 +61,24 @@ Route::get('regist', function () {
     return view('checking');
 });
 
+// route blog
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
 
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
+
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
+
+Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('formulir/proses', 'App\Http\Controllers\DosenController@proses');
+
+Route::get('/blog', function () {
+    return view('home');
+});
+
+Route::get('/blog/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/blog/kontak', function () {
+    return view('kontak');
+});
