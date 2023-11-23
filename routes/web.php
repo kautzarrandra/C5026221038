bc417cc4a51731bc3865037d064ca68c3d4e8ed4
Kautzar Randra Noor Khasyyatullah <kautzarrandra@gmail.com>
2023-11-23 14:55:11 +0700

    Pertemuan tgl 23 Nop 2023

--- routes/web.php
+++ routes/web.php
@@ -63,11 +63,8 @@ Route::get('regist', function () {
 
 // route blog
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
-
 Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
-
 Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
-
 Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
 Route::post('formulir/proses', 'App\Http\Controllers\DosenController@proses');
 
@@ -82,3 +79,11 @@ Route::get('/blog/tentang', function () {
 Route::get('/blog/kontak', function () {
     return view('kontak');
 });
+
+//route CRUS
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
